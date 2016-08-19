531fca4f24421d22b057c475760466773d70105d
br0r <viktor@silfverstrom.com>
2016-08-19 12:17:49 +0200

    Fixed so dirs are copied to .hydra

--- hydra.go
+++ hydra.go
@@ -2,19 +2,24 @@ package main
 
 import (
   "fmt"
+  "path"
   "os"
   "os/exec"
   "log"
   "io/ioutil"
 
+  "flag"
+
   "gopkg.in/yaml.v2"
 )
+
 
 const file_name string = ".hydra.yml"
 
 type Config struct {
   Services []struct {
-    Git string
+    Path string
+    Start string
     Config struct {
       Src string
       Dest string
@@ -23,6 +28,9 @@ type Config struct {
 }
 
 func main() {
+  var clean = flag.Bool("clean", false, "If we want a clean run")
+  flag.Parse()
+
   d, e := ioutil.ReadFile(file_name)
   if e != nil {
     log.Fatal(e)
@@ -36,16 +44,32 @@ func main() {
     os.Exit(1)
   }
 
+  if *clean {
+    _, e := exec.Command("rm", "-rf", ".hydra").Output()
+    if e != nil {
+      log.Fatal(e)
+    }
+  } 
+
   exec.Command("mkdir", ".hydra").Output()
 
   for i := 0; i < len(c.Services); i += 1 {
-    base := ".hydra/" + c.Services[i].Git
-    exec.Command("mkdir", base).Output()
-    u := base + "/" + c.Services[i].Config.Dest
-    fmt.Printf("%s\n", u)
-    _, err := exec.Command("touch", u).Output()
-    if err != nil {
-      log.Fatal(err)
+    p := c.Services[i].Path
+    _, name := path.Split(p)
+    base := path.Join(".hydra/", name)
+    if _, err := os.Stat(base); os.IsNotExist(err) {
+      fmt.Println("Creating", name)
+      _, e := exec.Command("cp", "-R", p, base).Output()
+      if e != nil {
+        log.Fatal(e)
+      }
+
+      u := path.Join(base, c.Services[i].Config.Dest)
+      fmt.Printf("%s\n", u)
+      _, err := exec.Command("touch", u).Output()
+      if err != nil {
+        log.Fatal(err)
+      }
     }
   }
 }
